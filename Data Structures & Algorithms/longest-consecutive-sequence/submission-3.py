7af72eedfc2df0bce53315fab06567ed4772dae2
Bhavana-Thumma <90683036+Bhavana-Thumma@users.noreply.github.com>
2026-05-25 18:48:01 -0500

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums.sort()
+        prev = 0
+        c = 0
+        pointer  = nums[0]
+        for i in range(len(nums)):
+            if(nums[i] == pointer):
+                c += 1
+                pointer += 1
+            elif(i > 0 and nums[i] == nums[i-1]):
+                continue
+            else:
+                if(prev < c):
+                    prev = c
+                c = 1
+                pointer = nums[i]+1
+        return max(prev, c)
