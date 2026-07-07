ad74c0c1e916422b9a8983c5651be67cad1e15cd
Bhavana-Thumma <90683036+Bhavana-Thumma@users.noreply.github.com>
2026-07-06 19:25:44 -0500

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.py
@@ -0,0 +1,26 @@
+from collections import defaultdict,deque
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for n1,n2 in edges:
+            graph[n1].append(n2)
+            graph[n2].append(n1)
+        visited = set()
+        components = 0
+        def bfs(node):
+            q = deque([node])
+            visited.add(node)
+            while(q):
+                nd = q.popleft()
+                for nei in graph[nd]:
+                    if nei not in visited:
+                        q.append(nei)
+                        visited.add(nei)
+                    
+        for node in range(n):
+            if node not in visited:
+                components+=1
+                bfs(node)
+        return components
+
+        
