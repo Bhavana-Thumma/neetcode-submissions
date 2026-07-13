0c29c2ad64a640668cb058529016205cbc63219f
Bhavana-Thumma <90683036+Bhavana-Thumma@users.noreply.github.com>
2026-07-13 13:11:08 -0500

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.py
@@ -0,0 +1,11 @@
+#Kadane's Algorithm
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum = nums[0]
+        maxSum = nums[0]
+        for i in range(1,len(nums)):
+            curSum=max(curSum+nums[i], nums[i])
+            maxSum = max(maxSum,curSum)
+        return maxSum
+
+        
